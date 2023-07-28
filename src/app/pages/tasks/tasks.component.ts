5ecc7d1aee93b9761dd3f9bc9d627042dcb0dc4e
Michael Pheasant <mike@pheasant.co.nz>
2023-07-28 11:50:48 +1000

    switch task/task2

--- src/app/pages/tasks/tasks.component.ts
+++ src/app/pages/tasks/tasks.component.ts
@@ -1,7 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
 import { DxDataGridComponent } from 'devextreme-angular';
 import 'devextreme/data/odata/store';
-import { InitializedEvent, dxDataGridColumn } from 'devextreme/ui/data_grid';
 
 @Component({
   templateUrl: 'tasks.component.html'
@@ -11,8 +10,7 @@ export class TasksComponent {
   @ViewChild(DxDataGridComponent, { static: true }) dataGrid: DxDataGridComponent;
   dataSource: any;
   priority: any[];
-
-  widthDate: number = 125;
+  widthDate: number;
 
   columnsChanging(e: any) {
     console.log("Colchange");
@@ -24,8 +22,8 @@ export class TasksComponent {
 
   constructor() {
 
-    this.customizeColumns = this.customizeColumns.bind(this);
-
+    this.widthDate = 80;
+    
     this.dataSource = {
       store: {
         type: 'odata',
@@ -55,81 +53,5 @@ export class TasksComponent {
   ngOnInit(): void {
     // at this point this.dataGrid.columns is not defined
   }
-
-  public onInitialized(e: InitializedEvent) {
-    console.debug(`> MJP - grid init 1`, this.dataGrid.columns.map((col: any) => [col.dataField, col.width, col.hidingPriority]));
-    setGridColumnHidingPriorities(this.dataGrid.columns as dxDataGridColumn[]);
-    // setDefaultGridColumnWidths(this.dataGrid.columns as any[], new Set(['Task_Status', 'Task_Completion', 'ResponsibleEmployee.Employee_Full_Name']));
-    console.debug(`> MJP - grid init 2`, this.dataGrid.columns.map((col: any) => [col.dataField, col.width, col.hidingPriority]));
-
-  };
-
-  customizeColumns(columns) {
-    console.debug(`> MJP - cust init 1`, columns.map((col: any) => [col.dataField, col.width, col.hidingPriority]));
-    setDefaultGridColumnWidths(columns as any[], new Set(['Task_Status', 'Task_Completion', 'ResponsibleEmployee.Employee_Full_Name']));
-    // debugger;
-  }
   
-}
-
-export const GridStandardWidths = {
-  special: 120,
-  date: 80,
-  number: 100,
-};
-
-export interface IGenericCol {
-  index: number;
-  dataField?: string;
-  dataType?: string;
-  name?: string;
-  width?: number | string;
-  visibleWidth: number;
-};
-
-function calcWidth(col: IGenericCol, specialFields: Set<string>): number | string | null {
-  if (typeof col.width === 'string') {
-    return col.width;
-  } 
-  if (typeof col.width === 'number') {
-    return col.width;
-  }
-  if (col.dataType === 'date' || col.dataType === 'number') {
-    return GridStandardWidths[col.dataType];
-  }
-  const name = col.dataField ?? col.name; 
-  if (name && specialFields?.has(name)) {
-    return GridStandardWidths.special;
-  }
-  return null;
-}
-
-export function setDefaultGridColumnWidths(columns: IGenericCol[], specialFields: Set<string>): void {
-  const pri = columns.length;
-  columns.forEach((col, i) => {
-    if (typeof col === 'string') {
-      console.error(`invalid column type - expected Column`);
-    } else {
-      const width = calcWidth(col, specialFields);
-      if (width !== null) {
-        console.debug(`> set width [${width}] [${col.dataField ?? col.name}]`)
-        col.width = width;
-      } else {
-        console.debug(`> leave width [${col.width}] [${col.dataField ?? col.name}]`)
-      }
-    }
-  });
-}
-
-export function setGridColumnHidingPriorities(columns: { hidingPriority?: number }[]): void {
-  // Column hiding values must be unique (if not, one column with duplicate priority will not disappear)
-  // Column hiding priority low values means hide first
-  const pri = columns.length;
-  columns.forEach((col, i) => {
-    if (typeof col === 'string') {
-      console.error(`invalid column type - expected Column`);
-    } else {
-      col.hidingPriority = pri - i;
-    }
-  });
 }
